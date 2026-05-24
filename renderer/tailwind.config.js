f559a37fdb379fcf5020e8f78e1b590be16e61c5
Johnathan Vu <jonathanvu11@gmail.com>
2026-05-23 23:27:05 -0400

    fix tailwind config extension to .js so postcss can read custom colors

---
+++ renderer/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './lib/**/*.{ts,tsx}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        paper: {
+          50: '#faf9f7',
+          100: '#f2f0ec',
+          200: '#e8e4dd',
+          900: '#1c1917',
+        },
+      },
+      fontFamily: {
+        sans: ['var(--font-inter)', 'system-ui', 'sans-serif'],
+        serif: ['var(--font-lora)', 'Georgia', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
